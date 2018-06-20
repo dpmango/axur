e7022f122d646fc46e134c1abb23852e73c64209
Sergey Khmelevskoy <xs290@me.com>
2018-06-21 01:47:01 +0300

    fixed gulp task for copy

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -21,7 +21,7 @@ gulp.task('copy:rootfiles', function() {
 
 gulp.task('copy:animations', function() {
   return gulp
-    .src(config.src.root + '/inline-animations/*.*')
+    .src(config.src.root + '/inline-animations/**/*.*')
     .pipe(gulp.dest(config.dest.root + '/inline-animations'));
 });
 
@@ -33,7 +33,7 @@ gulp.task('copy', [
 ]);
 
 gulp.task('copy:watch', function() {
-  gulp.watch(config.src.root + '/inline-animations/*.*', ['copy:animations']);
+  gulp.watch(config.src.root + '/inline-animations/**/*.*', ['copy:animations']);
   gulp.watch(config.src.vendor + '/**/*.*', ['copy:vendor']);
   gulp.watch(config.src.root + '/*.*', ['copy:rootfiles']);
 });
